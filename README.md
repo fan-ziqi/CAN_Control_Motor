d283123feb283bada02a6af082e80dd292faf88a
范子琦 <1365479985@qq.com>
2021-05-15 23:59:00 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CAN_Control_Motor
+ CAN总线控制GM6020电机，pid控制模拟多档开关
---
+++ application/pid.h
@@ -0,0 +1,40 @@
+
+#ifndef PID_H
+#define PID_H
+#include "struct_typedef.h"
+enum PID_MODE
+{
+    PID_POSITION = 0,
+    PID_DELTA
+};
+
+typedef struct
+{
+    uint8_t mode;
+    //PID 三参数
+    fp32 Kp;
+    fp32 Ki;
+    fp32 Kd;
+
+    fp32 max_out;  //最大输出
+    fp32 max_iout; //最大积分输出
+
+    fp32 set;
+    fp32 fdb;
+
+    fp32 out;
+    fp32 Pout;
+    fp32 Iout;
+    fp32 Dout;
+    fp32 Dbuf[3];  //微分项 0最新 1上一次 2上上次
+    fp32 error[3]; //误差项 0最新 1上一次 2上上次
+
+} pid_type_def;
+
+extern void PID_init(pid_type_def *pid, uint8_t mode, const fp32 PID[3], fp32 max_out, fp32 max_iout);
+
+extern fp32 PID_calc(pid_type_def *pid, fp32 ref, fp32 set);
+
+extern void PID_clear(pid_type_def *pid);
+
+#endif
